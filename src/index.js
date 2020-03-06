d8970e0965565cd8e5de2768329ad80f4139f8dd
Faisal Karim <faisalkarim96@gmail.com>
2020-03-06 18:18:50 +0700

    Add redux configuration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from "redux";
+import { Provider} from 'react-redux';
+import counterReducer from './store/reducers/counter';
+import resultReducer from "./store/reducers/result";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+})
+const store = createStore(rootReducer) /* Reducer */
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
